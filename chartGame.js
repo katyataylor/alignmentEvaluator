b603f93e428686b753d762c7e101550e7efea0a3
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-23 14:13:48 -0500

    Initial functional multi-slider prototype

--- chartGame.js
+++ chartGame.js
@@ -1,6 +1,28 @@
-// window.addEventListener(`DOMContentLoaded`, () => {
+window.addEventListener(`DOMContentLoaded`, () => {
     
-// });
-const playerDot = document.querySelector(`.dot`);
-playerDot.style.left = "50%";
-playerDot.style.top = "50%";
+    // 1.Grabs elements AFTER page loads
+    const playerDot = document.querySelector(`.dot`);
+    const xSlider = document.querySelector(`.x-slider`);
+    const ySlider = document.querySelector(`.y-slider`);
+
+    // Default visual starting pos
+    playerDot.style.left = "50%";
+    playerDot.style.top = "50%";
+
+    // Listen for X-axis changes
+    xSlider.addEventListener("input", () => {
+        let currentScore = xSlider.value;
+        let xScore = currentScore;
+        let xPercentage = (xScore / 10) * 100;
+        playerDot.style.left = `${xPercentage}%`;
+    });
+
+    // Listen for Y-axis changes
+    ySlider.addEventListener("input", () => {
+        let currentScore = ySlider.value;
+        let yScore = currentScore;
+        let yPercentage = (yScore / 10) * 100;
+        playerDot.style.top = `${yPercentage}%`;
+    });
+
+});
